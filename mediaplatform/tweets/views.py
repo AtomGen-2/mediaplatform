d2ab52b736f90fd41c652edab1e16fc0e86ef1e2
AtomGen-2 <gaonkarsvedant.471@gmail.com>
2021-06-30 10:02:44 +0530

    XML requests

--- mediaplatform/tweets/views.py
+++ mediaplatform/tweets/views.py
@@ -10,7 +10,8 @@ def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
     tweets_list = [{"id":x.id, "content": x.content} for x in qs]
     data = { 
-        "response" : tweets_list
+        "isUser" : False, 
+        "response" : tweets_list, 
     }
     return JsonResponse(data)
 
